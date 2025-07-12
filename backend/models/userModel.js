f75c4105050fcd3471e7a34223cb1ecd6819d4c1
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-07-12 18:55:11 +0530

    Encrypting passwords, Registering new user & Logging in existing user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,5 +33,20 @@ const userSchema = mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema)
-module.exports = User
+// Comparing the encrypted password & password entered by user
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypting the password before saving to DB
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,12 @@ const express = require("express"); // Import express module, a web framework fo
 const scribblesData = require("./data/scribbles"); // Import sample scribbles
 const connectDB = require("./config/db"); // Import fn to connect to mongoDB
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express(); // Create an Express application instance
 connectDB(); // Connect to mongoDB
+
 app.use(express.json()); // Adding middleware express.json() to access data from req body
-
-const PORT = process.env.PORT || 5000; // Define port from .env file (defaults to 5000)
-
-// Start server and log a message once it's running
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 
 // GET route for root path
 app.get("/", (req, res) => {
@@ -36,3 +33,12 @@ app.get("/api/scribbles/:id", (req, res) => {
 
 // api routes for users
 app.use("/api/users", userRoutes);
+
+// Adding custom middleware for error handling
+app.use(notFound); 
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000; // Define port from .env file (defaults to 5000)
+
+// Start server and log a message once it's running
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
