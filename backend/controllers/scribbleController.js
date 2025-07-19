1c1639f9ae96d8fda2c63d0dc33e0e872d96e974
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-07-19 17:14:10 +0530

    Created APIs for fetching, editing & deleting scribble by id

--- backend/controllers/scribbleController.js
+++ backend/controllers/scribbleController.js
@@ -27,7 +27,77 @@ const createScribble = asyncHandler(async (req, res) => {
   }
 });
 
+const getScribbleById = asyncHandler(async (req, res) => {
+  // Find the scribble by id
+  const scribble = await Scribble.findById(req.params.id);
+
+  if (scribble) {
+    // Check if the scribble belongs to currently logged in user
+    if (scribble.user.toString() !== req.user._id.toString()) {
+      res.status(401);
+      throw new Error("You cannot access this scribble!");
+    }
+    res.json(scribble);
+  } else {
+    res.status(404).json({ message: "Scribble not found!" });
+  }
+});
+
+const editScribble = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  // Check if all fields have data
+  if ((!title, !content, !category)) {
+    res.status(400);
+    throw new Error("Please fill all the fields!");
+  }
+
+  // Find the scribble by id
+  const scribble = await Scribble.findById(req.params.id);
+
+  // Check if the scribble belongs to currently logged in user
+  if (scribble.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You cannot perform this action!");
+  }
+
+  // Update the scribble
+  if (scribble) {
+    scribble.title = title;
+    scribble.content = content;
+    scribble.category = category;
+    const updatedScribble = await scribble.save();
+    res.json(updatedScribble);
+  } else {
+    res.json(404);
+    throw new Error("Scribble not found!");
+  }
+});
+
+const deleteScribble = asyncHandler(async (req, res) => {
+  // Find the scribble by id
+  const scribble = await Scribble.findById(req.params.id);
+
+  // Check if the scribble belongs to currently logged in user
+  if (scribble.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You cannot perform this action!");
+  }
+
+  // Remove the scribble
+  if (scribble) {
+    await scribble.deleteOne();
+    res.json({ message: "Scribble removed!" });
+  } else {
+    res.json(404);
+    throw new Error("Scribble not found!");
+  }
+});
+
 module.exports = {
   getAllScribbles,
   createScribble,
+  getScribbleById,
+  editScribble,
+  deleteScribble,
 };
--- backend/routes/scribbleRoutes.js
+++ backend/routes/scribbleRoutes.js
@@ -3,11 +3,17 @@ const { protect } = require("../middlewares/authMiddleware");
 const {
   getAllScribbles,
   createScribble,
+  getScribbleById,
+  editScribble,
+  deleteScribble,
 } = require("../controllers/scribbleController");
 
 const router = express.Router();
 
 router.route("/").get(protect, getAllScribbles);
 router.route("/create").post(protect, createScribble);
+router.route("/:id").get(protect, getScribbleById);
+router.route("/edit/:id").put(protect, editScribble);
+router.route("/delete/:id").delete(protect, deleteScribble);
 
 module.exports = router;
