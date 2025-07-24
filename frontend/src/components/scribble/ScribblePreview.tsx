3d8a187de6327dddaabfcaf4bd8407c010e6f6f6
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-07-24 17:00:22 +0530

    Added typography tailwind plugin to use markdown styling & Refactored ScribblePreview

--- frontend/src/components/scribble/ScribblePreview.tsx
+++ frontend/src/components/scribble/ScribblePreview.tsx
@@ -1,24 +1,31 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useScribble } from "../../context/ScribbleContext";
+import ReactMarkdown from "react-markdown";
 // types
 import type { ScribbleData } from "../../utils/types";
 
 const ScribblePreview = () => {
-  const [selectedScribble, setSelectedScribble] = useState<ScribbleData | undefined>(undefined);
-  
+  const [selectedScribble, setSelectedScribble] = useState<
+    ScribbleData | undefined
+  >(undefined);
+
   const params = useParams();
   const { scribbles } = useScribble();
 
+  // Currently selected scribble from list of all scribbles
   useEffect(() => {
-    const currentScribble = scribbles.find((scribble) => scribble._id === params.id);
+    const currentScribble = scribbles.find(
+      (scribble) => scribble._id === params.id
+    );
     if (currentScribble) {
       setSelectedScribble(currentScribble);
     } else {
-      setSelectedScribble(undefined)
+      setSelectedScribble(undefined);
     }
   }, [scribbles, params.id]);
 
+  // If no scribble is selected, display fallback UI
   if (!selectedScribble) {
     return (
       <div>
@@ -33,7 +40,13 @@ const ScribblePreview = () => {
       <span className="bg-bluePrimary px-4 py-1 rounded-md text-white w-fit">
         {selectedScribble.category}
       </span>
-      <p className="">{selectedScribble.content}</p>
+      {/* 
+        In order to allow the markdown syntax to provide the styles prevent tailwind.css from overwriting it, I have installed a package called @tailwindcss/typography.
+        This package provides a prose class, which removes the tailwind styles.
+      */}
+      <div className="prose dark:prose-invert overflow-y-scroll pr-2 max-w-none w-full">
+        <ReactMarkdown>{selectedScribble.content}</ReactMarkdown>
+      </div>
     </div>
   );
 };
